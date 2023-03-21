7ad65a0017b1caba3cbb88f1e4a131abd3ea2a5a
ademten7 <ademten7@gmail.com>
2023-03-21 22:04:02 +0100

    added HTML report

---
+++ cypress/e2e/1-getting-started/20.GenerateHTMLREports.cy.js
@@ -0,0 +1,33 @@
+//1- npm i cypress-mochawesome-reporter
+/*
+2- Change cypress reporter & setup hooks
+
+Edit config file (cypress.config.js by default)
+
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+});
+*/
+
+/*
+-to create a HTML report
+npx cypress run 
+-to run specific test
+npx cypress run --spec <relative part of file>
+it generates an report folder. inside it there is a index.html==> copy absolute part and  past it on browser
+this is the hedless test
+*/
+
+/*
+npx cypress run --spec <relative part of file> --browser chrome   ===> this headless
+npx cypress run --spec <relative part of file> --browser chrome   ===>  this is headed
+
+*/
+let i = 5
